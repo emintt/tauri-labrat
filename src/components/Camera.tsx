44e35ffff003ed5729c5265a29bd6e900b0263f0
liem nguyen <elminguyen@gmail.com>
2025-01-15 09:40:36 +0200

    add Camera component for video input handling

---
+++ src/components/Camera.tsx
@@ -0,0 +1,32 @@
+import { forwardRef, useEffect, useImperativeHandle, useRef } from "react";
+
+type CameraProps = {
+  width: number;
+  aspect: number;
+}
+
+const Camera = forwardRef<HTMLVideoElement, CameraProps>((props, ref) => {
+
+  const { width, aspect } = props;
+  const height = width / aspect;
+  
+  const videoRef = useRef<HTMLVideoElement>(null); // Reference to the video element
+
+  // jaetaan videoRef parentille (lähetetäään refin kanssa videoRef)
+  useImperativeHandle(ref, () => videoRef.current!);
+
+  useEffect(() => {
+    const setupVideoInput = async () => {
+      // getUserMedia
+      videoRef.current?.srcObject = stream;
+    };
+    setupVideoInput;
+  }, []);
+  
+  return <video ref={videoRef} width={width} height={height} />
+
+
+
+});
+
+export default Camera;
--- src/views/DetectFace.tsx
+++ src/views/DetectFace.tsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useRef, useState } from 'react';
 
 import * as faceapi from 'face-api.js';
+import { Camera } from 'lucide-react';
 
 const DetectFace: React.FC = () => {
 const videoRef = useRef<HTMLVideoElement>(null); // Reference to the video element
@@ -68,16 +69,7 @@ let timer: ReturnType<typeof setTimeout> | null = null;
 return (
     <div style={{ textAlign: 'center', marginTop: '20px', position: 'relative' }}>
        <h1>Face Detection</h1>
-        <video
-            ref={videoRef}
-            autoPlay
-            muted
-            playsInline
-            style={{
-                width: '80%',
-                border: '1px solid black',
-            }}
-        />
+        <Camera ref={videoRef} width={800} aspect={ 16 / 9} />
         {detection && (
             <div
                 style={{
