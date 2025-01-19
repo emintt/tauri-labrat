7e3407617b87cc2207a5bc033829d996a280554f
liem nguyen <elminguyen@gmail.com>
2025-01-19 23:36:18 +0200

    update

---
+++ src/hooks/FaceHooks.ts
@@ -0,0 +1,56 @@
+import { RefObject, useEffect, useState } from "react";
+import * as faceapi from 'face-api.js';
+import randomstring from "@/lib/randomstring";
+
+const useFaceDetection = () => {
+  const [detection, setDetection] = useState<faceapi.FaceDetection | null>(null); // Detected face
+
+  useEffect(() => {
+    // Load the face detection models
+    const loadModels = async () => {
+      try {
+        await faceapi.nets.tinyFaceDetector.loadFromUri('/models');
+        await faceapi.nets.faceLandmark68TinyNet.loadFromUri('/models');
+        await faceapi.nets.faceRecognitionNet.loadFromUri('/models');
+        console.log('Models loaded');
+      } catch (error) {
+        console.error('Error loading models:', error);
+      }
+    };
+
+    loadModels();
+  }, []);
+
+  const getDescriptors = async ( videoRef: RefObject<HTMLVideoElement> ) => {
+    if (!videoRef.current) {
+      return;
+    }
+
+    const result = await faceapi
+      .detectSingleFace(
+        videoRef.current, 
+        new faceapi.TinyFaceDetectorOptions(),
+      )
+      .withFaceLandmarks(true)
+      .withFaceDescriptor();
+    
+    if (!result) {
+      console.error("No face detected");
+      return;
+    }
+
+
+    const faceName = randomstring(6);
+    const labeledDescriptor = new faceapi.LabeledFaceDescriptors(faceName, [
+      result.descriptor,
+    ]);
+
+    console.log('result', labeledDescriptor);
+
+    setDetection(result.detection);
+  };
+  return { detection, getDescriptors };
+    
+};
+
+export { useFaceDetection };
--- src/views/DetectFace.tsx
+++ src/views/DetectFace.tsx
@@ -1,35 +1,23 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React, { useEffect, useRef } from 'react';
 
-import * as faceapi from 'face-api.js';
 import Camera from '@/components/Camera';
+import { useFaceDetection } from '@/hooks/FaceHooks';
 
 const DetectFace: React.FC = () => {
 const videoRef = useRef<HTMLVideoElement>(null); // Reference to the video element
-const [detection, setDetection] = useState<faceapi.FaceDetection | null>(null); // Detected face
+// const [detection, setDetection] = useState<faceapi.FaceDetection | null>(null); // Detected face
+const { detection, getDescriptors } = useFaceDetection();
 
 useEffect(() => {
 let timer: ReturnType<typeof setTimeout> | null = null;
 
-    // Load the face detection models
-    const loadModels = async () => {
-      try {
-        await faceapi.nets.tinyFaceDetector.loadFromUri('/models');
-        console.log('Models loaded');
-      } catch (error) {
-        console.error('Error loading models:', error);
-      }
-    };
-
     // Detect face from video frames
     const detectFace = async () => {
-      if (videoRef.current) {
-        const result = await faceapi.detectSingleFace(
-          videoRef.current,
-          new faceapi.TinyFaceDetectorOptions()
-        );
-
-        setDetection(result || null); // Update detection state
-      }
+      try {
+        await getDescriptors(videoRef);
+      } catch (error) {
+        console.error("Error detecting face:", error);
+      };
 
       // Schedule the next detection
       timer = setTimeout(detectFace, 100);
@@ -57,7 +45,8 @@ let timer: ReturnType<typeof setTimeout> | null = null;
       }
     };
 
-    loadModels().then(startDetection); // Load models and start detection
+    startDetection();
+    // loadModels().then(startDetection); // Load models and start detection
 
     // Cleanup on unmount
     return () => {
@@ -66,23 +55,27 @@ let timer: ReturnType<typeof setTimeout> | null = null;
 
 }, []);
 
+// console.log("Detection object", detection);
+
 return (
-    <div style={{ textAlign: 'center', marginTop: '20px', position: 'relative' }}>
-       <h1>Face Detection</h1>
-        <Camera ref={videoRef} width={800} aspect={ 16 / 9} />
-        {detection && (
-            <div
-                style={{
-                    position: 'absolute',
-                    top: detection.box.y,
-                    left: detection.box.x,
-                    width: detection.box.width,
-                    height: detection.box.height,
-                    border: '2px solid red',
-                    pointerEvents: 'none',
-                }}
-            ></div>
-        )}
+    <div style={{ textAlign: 'center', marginTop: '20px' }}>
+        <h1>Face Detection</h1>
+        <div style={{ position: 'relative' }} >
+          <Camera ref={videoRef} width={800} aspect={ 16 / 9} />
+          {detection && (
+              <div
+                  style={{
+                      position: 'absolute',
+                      top: detection.box.y,
+                      left: detection.box.x,
+                      width: detection.box.width,
+                      height: detection.box.height,
+                      border: '2px solid red',
+                      pointerEvents: 'none',
+                  }}
+              ></div>
+          )}
+        </div>
     </div>
     );
 };
